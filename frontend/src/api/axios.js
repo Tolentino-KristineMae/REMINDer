60e2add02daeae342a9daebce2d0220c0451cf9c
Tolentino-KristineMae <maemaemaetolentino@gmail.com>
2026-03-21 02:36:45 +0800

    try

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -21,7 +21,6 @@ const API_BASE_URL = (() => {
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
-        'Content-Type': 'application/json',
         'Accept': 'application/json',
     },
 });
