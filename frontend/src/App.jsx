1461139a69bbf8d36e4f367d2aee874a8833dc6f
Tolentino-KristineMae <maemaemaetolentino@gmail.com>
2026-03-21 22:59:42 +0800

    sec

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ const DateTimeDisplay = () => {
     }, []);
 
     const formatTime = (date) => {
-        return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
+        return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
     };
 
     const formatDate = (date) => {
